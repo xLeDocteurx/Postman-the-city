78f1a2a134e55699b113fc4c881db339f63bf26d
Gyozmo <binantguillaume@gmail.com>
2018-10-24 15:19:53 +0200

    buttons to generate cities

--- scripts.js
+++ scripts.js
@@ -8,7 +8,20 @@ let city = new City()
 // Déclenchement de la méthode d'initialisation de la ville
 city.init(streets_number, houses_number, residents_number, letters_number)
 // Créer un report pour pouvoir surveiller la génération de la ville
+
+
+
+const report = () => {
 Generate.report()
+city.init(streets_number, houses_number, residents_number, letters_number)
+}
+
 // Déclenchement du facteur
-city.postman.get_mail_from_postoffice()
-city.postman.start_tour()
+
+const getMail = () => {
+    city.postman.get_mail_from_postoffice()
+}
+
+const startTour = () => {
+    city.postman.start_tour()
+}
