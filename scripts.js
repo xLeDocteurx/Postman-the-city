2c9827cc0bb42d6f1ca5bb9efd3f26946e8061ea
xLeDocteurx <lenoirc.fr@gmail.com>
2019-04-01 22:10:40 +0200

    push mid fonctionnality for formatting computer reasons

--- scripts.js
+++ scripts.js
@@ -1,4 +1,4 @@
-let streets_number = document.getElementById("setStreetsNumber").value
+let streets_number = 20
 const houses_number = streets_number * 5
 const residents_number = Math.round(houses_number * 1.25)
 let letters_number = Math.round(Math.random() * residents_number)
@@ -6,11 +6,15 @@ let letters_number = Math.round(Math.random() * residents_number)
 // Instanciation de la nouvelle ville
 let city = new City()
 // Déclenchement de la méthode d'initialisation de la ville
-city.init(streets_number, houses_number, residents_number, letters_number)
+// city.init(streets_number, houses_number, residents_number, letters_number)
 // Créer un report pour pouvoir surveiller la génération de la ville
 const report = () => {
 Generate.report()
 city.init(streets_number, houses_number, residents_number, letters_number)
+console.log(streets_number)
+console.log(houses_number)
+console.log(residents_number)
+console.log(letters_number)
 }
 
 // Déclenchement du facteur
